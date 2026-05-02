90543d04e03f133cf34d4bb2de0373a3c9f59668
danicalde2003 <danicalde2003@gmail.com>
2026-05-02 17:28:43 -0600

    Cambios en el archivo de configuración para actualizar en la URL del backend

--- js/config.js
+++ js/config.js
@@ -4,6 +4,7 @@
  * En producción: cambia API_BASE y API_ORIGIN por la URL de tu backend
  * (ej: https://api.tudominio.com/api y https://api.tudominio.com).
  */
+
 (function () {
   'use strict';
   var isLocal = /localhost|127\.0\.0\.1/i.test(window.location.hostname);
